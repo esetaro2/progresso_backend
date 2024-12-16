cf950aabf349a99c2f3f9626a4b7e0154a2843ae
esetaro2 <bbot3333@gmail.com>
2024-12-16 14:46:03 +0100

    Update Role entity and RoleRepository to comply with checkstyle rules

--- src/main/java/com/progresso/backend/repository/RoleRepository.java
+++ src/main/java/com/progresso/backend/repository/RoleRepository.java
@@ -1,7 +1,6 @@
 package com.progresso.backend.repository;
 
 import com.progresso.backend.model.Role;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
