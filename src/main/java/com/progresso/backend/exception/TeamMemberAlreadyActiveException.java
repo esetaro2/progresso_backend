cbbfbf45d5425e98e4f758ce304a467f23c3a945
esetaro2 <bbot3333@gmail.com>
2024-12-28 21:02:23 +0100

    Delete MaxActiveTeamsExceededException.java, TeamMemberAlreadyActiveException.java

--- src/main/java/com/progresso/backend/exception/TeamMemberAlreadyActiveException.java
+++
@@ -1,8 +0,0 @@
-package com.progresso.backend.exception;
-
-public class TeamMemberAlreadyActiveException extends RuntimeException {
-
-  public TeamMemberAlreadyActiveException(String message) {
-    super(message);
-  }
-}
