3730a26b224bbb9fbd01927e5f7ee7ceac953046
esetaro2 <bbot3333@gmail.com>
2025-02-14 16:18:15 +0100

    Remove TaskNameAlreadyExistsException and TeamNameAlreadyExistsException classes

--- src/main/java/com/progresso/backend/exception/TaskNameAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.progresso.backend.exception;
-
-public class TaskNameAlreadyExistsException extends RuntimeException {
-
-  public TaskNameAlreadyExistsException(String message) {
-    super(message);
-  }
-}
--- src/main/java/com/progresso/backend/exception/TeamNameAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.progresso.backend.exception;
-
-public class TeamNameAlreadyExistsException extends RuntimeException {
-
-  public TeamNameAlreadyExistsException(String message) {
-    super(message);
-  }
-}
